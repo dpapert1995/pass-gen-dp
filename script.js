567379934a8663921ba4b00d7e176eb4062bf789
Daniel Papert <dpapert@gmail.com>
2022-08-09 00:11:08 -0400

    Added functions for fufilling password criteria.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +14,27 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Lowercase letters criteria is set to true by default, so this is ran no matter what.//
+function addLowercase() {
+  const lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  return lowercase;
+}
+
+// Uppercase letter criteria calls upon lowercase letter function.//
+function addUppercase() {
+  const upperCase = addLowercase().toString().toUpperCase().split(",");
+  return upperCase;
+}
+
+// Function to add numbers if criteria is selected. //
+function addNumbers() {
+  const numbers = [0,1,2,3,4,5,6,7,8,9];
+  return numbers;
+}
+
+// Function to add special characters if criteria is selected. //
+function addSpecialChars() {
+  const specialChar = ['!', '"', '#', "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "]", "\\", "]", "^", "_", "`","{", "|", "}", "~"];
+  return specialChar;
+}
